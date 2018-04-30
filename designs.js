d10a3cddf69ce2f627f103e3fb3fbde1e622e2fb
Gitty Zazon <gittyzazon@gmail.com>
2018-04-30 04:31:05 -0400

    Update functionality of toggle coloring, Add comments

--- designs.js
+++ designs.js
@@ -1,4 +1,4 @@
-// Select color input
+/ Select color input
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
@@ -7,30 +7,35 @@ $(function(){
   //creates the grid based on user input
   $('#sizePicker').submit(function makeGrid(evt) {
     evt.preventDefault();
+    //Clear previous grid
     $('#pixel_canvas').empty();
-    var row = $('#input_height').val();
-    var cell = $('#input_width').val();
-    var table = $('#pixel_canvas');
-    for(var r = 0; r < row; r++){
+    //Don't begin dragging color automatically
+    $('#pixel_canvas').removeClass('colorMe')
+    let row = $('#input_height').val();
+    let cell = $('#input_width').val();
+    const table = $('#pixel_canvas');
+    //Add rows and cells
+    for(let r = 0; r < row; r++){
       table.append("<tr></tr>").css('box-shadow',"0 0 5px 2px black")
-      for(var c = 0; c < cell; c++){
+      for(let c = 0; c < cell; c++){
         table.children().last().append('<td></td>');
       }
     }
    });
   
    //function to use selected color and apply to cell
-   //click to begin color
-   $('#pixel_canvas').on('click','td', function(){
-      var color = $('#colorPicker').val();
-      $('#pixel_canvas').on('mouseover','td', function(){
-        $(this).css('background-color', color);
-      //click to end color
-      $('#pixel_canvas').on('click', function(){
-        $(this).off('mouseover', 'td');
+   //click to toggle coloring ability
+   $('#pixel_canvas').on('click', function(){
+      $(this).toggleClass('colorMe');
+      let color = $('#colorPicker').val();
+      if ($(this).hasClass('colorMe') === true) {
+        $('#pixel_canvas').on('mouseover','td', function(){
+            $(this).css('background-color', color);
         });
-      });  
-   });
+      } else {
+          $(this).off('mouseover', 'td');
+      };
+    });
   
    //function to clear a cell on double click
    $("#pixel_canvas").on('dblclick','td', function(){
